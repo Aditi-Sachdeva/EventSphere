ed10d9d97b2026a3107d2766b4144ae51f85e1b9
Ananya Sharma <ananyashar06@gmail.com>
2026-03-05 09:24:07 +0530

    Added Home Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,40 @@
+// import { Routes, Route, Navigate } from "react-router-dom";
+// import Login from "./pages/Login";
+// import SignUp from "./pages/SignUp";
+
+// function App() {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<Login />} />
+//       <Route path="/signup" element={<SignUp />} />
+
+//       <Route
+//         path="/dashboard"
+//         element={
+//           <div className="min-h-screen flex items-center justify-center text-4xl font-bold">
+//             Dashboard 🎉
+//           </div>
+//         }
+//       />
+
+//       <Route path="*" element={<Navigate to="/" />} />
+//     </Routes>
+//   );
+// }
+
+// export default App;
+
+
 import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/" element={<Home />} />   {/* 👈 CHANGE HERE */}
+      <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
 
       <Route
